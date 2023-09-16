80eddc593628f48fd695f27d46d2f01e72c1242b
Clemente Solorio <solorioeclem164@gmail.com>
2023-09-16 14:56:24 -0700

    docs update + better search examples

--- src/components/animate-searches.tsx
+++ src/components/animate-searches.tsx
@@ -6,14 +6,30 @@ export function AnimateSearches() {
   return (
     <TypeAnimation
       sequence={[
-        "Find all courses relating to natural landmarks",
-        1000,
-        "Find all courses relating to human history",
-        1000,
-        "Find all courses relating to entrepreneurship",
-        1000,
-        "Find all courses relating to game development",
-        1000,
+        "Learn how natural landmarks are created",
+        1500,
+        "Learn how games are developed",
+        1500,
+        "Learn how financial markets operate",
+        1500,
+        "Learn how humans developed cultures",
+        1500,
+        "Learn how black holes form",
+        1500,
+        "Learn how websites are built",
+        1500,
+        "Learn how life evolves naturally",
+        1500,
+        "Learn how to craft narratives",
+        1500,
+        "Learn how crime affects society",
+        1500,
+        "Learn how therapy aids individuals",
+        1500,
+        "Learn how religions shape cultures",
+        1500,
+        "Learn how fitness enhances health",
+        1500,
       ]}
       repeat={Infinity}
       className="after:text-orange-500 after:content-['|']"
